d0443515d41a4826179c774f05ba57b631c54aa2
Ghulam Mustfa <mustfa18gm@gmail.com>
2024-12-05 14:24:15 +0500

    changes made

---
+++ Conditional statements/question15.py
@@ -0,0 +1,9 @@
+print("Range is 0 to 9999")
+num = int(input("Enter a number: "))
+lower_limit = 0
+upper_limit = 9999
+
+if lower_limit <= num <= upper_limit:
+    print("The number is within the range.")
+else:
+    print("The number is not within the range.")
--- loops/question12.py
+++ loops/question12.py
@@ -1,8 +1,12 @@
-# print("Prime numbers between 1 and 50 are : ")
-# for num in range(2,51):
-#     for prime in range(2,num):
-#         if num % prime == 0:
-#             print(" ")
-#         else:
-#             break
-#     print(num)
+print("Prime numbers between 1 and 50 are:")
+
+# 1 is not a prime number.
+
+for num in range(2, 51):
+    is_prime = True 
+    for i in range(2, int(num ** 0.5) + 1): 
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+          print(num, end=" ")
